2d9085844be05590aeba5f8ca7f562e4d699f611
MayronGMZ <mayron.souza@gmz.solutions>
2020-04-15 13:30:55 -0300

    Implementando o CRUD

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,56 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.json({ message: 'Hello world.' }))
+const projects = []
 
-app.listen(3333, () => console.log('Server running...'))
+app.get('/projects', (req, res) => {
+
+  return res.json(projects)
+})
+
+app.post('/projects', (req, res) => {
+  const { title, owner } = req.body
+
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project)
+
+  return res.status(201).json(project)
+})
+
+app.put('/projects/:id', (req, res) => {
+  const { id } = req.params
+  const { title, owner } = req.body
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return res.status(404).json({ error: 'Project not found.' })
+  }
+
+  const project = { id, title, owner }
+
+  projects[projectIndex] = project
+
+  return res.json(project)
+})
+
+app.delete('/projects/:id', (req, res) => {
+  const { id } = req.params
+
+  const projetcIndex = projects.findIndex(project => project.id === id)
+
+  if (projetcIndex < 0) {
+    return res.status(404).json({ error: 'Project not found' })
+  }
+
+  projects.splice(projetcIndex, 1)
+
+  return res.status(204).send()
+})
+
+app.listen(3333, () => console.log('🚀️ Server running...'))
